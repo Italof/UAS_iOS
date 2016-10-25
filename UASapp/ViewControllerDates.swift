f409d0b11dd083f6a08bde092564a49d73d95688
jameschirinos <jamesj2809@gmail.com>
2016-10-24 21:24:01 -0500

    Table view done

---
+++ UASapp/ViewControllerDates.swift
@@ -0,0 +1,61 @@
+//
+//  ViewControllerDates.swift
+//  UASapp
+//
+//  Created by inf227al on 24/10/16.
+//  Copyright © 2016 sumajg. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerDates: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet var Dates: UITableView!
+    
+    var datesA = ["12/07/2012","12/09/2016"]
+    var times = ["12:00 pm","12:10 pm"]
+    var themes = ["Academico","Academico"]
+    var students = ["Pedro Perez","Juan Perez"]
+    var statusA = ["Pendiente","Pendiente"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return datesA.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let  cell = self.Dates.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCellDate
+        
+        cell.date.text = datesA[indexPath.row]
+        cell.time.text = times[indexPath.row]
+        cell.theme.text = themes[indexPath.row]
+        cell.student.text = students[indexPath.row]
+        cell.status.text = statusA[indexPath.row]
+        
+        return cell
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
